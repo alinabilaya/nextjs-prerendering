5cbee336e1f995c3a4a4916768c82e1d050b6fca
Alina Bilaya <10671738+alinabilaya@users.noreply.github.com>
2021-08-31 16:57:10 +0300

    Example how to avoid hydration warning

--- pages/erroronrendering.js
+++ pages/erroronrendering.js
@@ -1,23 +1,32 @@
 import Button from "../components/Button";
 import MainContainer from "../components/MainContainer";
 import CustomLink from "../components/CustomLink";
+import { useState, useEffect } from 'react'
 
-const renderComponent = () => {
-    if (process.browser) {
+function renderComponent (showButton) {
+    if (showButton) {
       return <Button text={"Custom button"} />;
     }
     else {
-      return <CustomLink href={'/'} text={"Home link"}/>;
+      return <CustomLink href={'/'} text={"Home link"} />;
     }
   }
 
 const ErrorOnRendering = () => {
+    // This page is an example of how to avoid warning when server rendering result is not the same as one on client during hydration.
+    // Q: But can I just check if window is available? A: Well, it depends, but don’t do this!
+    // Note that this approach will make your components slower because they have to render twice, so use it with caution.
+    // Also you can use suppressHydrationWarning prop on the element.
+    const [showButton, setshowButton] = useState(false)
+    useEffect(() => setshowButton(process.browser), [])
+    console.log("component is rendered")
+
     return (
         <div>
             <MainContainer>
                 <h1>Welcome to ErrorOnRendering page!</h1>
                 <p>This page contains error on re-rendering due to different rendering results for html and nextJs</p>
-                <div>{renderComponent()}</div>
+                <div>{renderComponent(showButton)}</div>
             </MainContainer>
         </div>
     );
